e0bf91c70329415ea3d040d143b746556fa6fa96
Janmou <160777089+Janmou@users.noreply.github.com>
2024-08-22 16:08:13 -0300

    Add files via upload

---
+++ atividade 13/Atividade_13.js
@@ -0,0 +1,14 @@
+var alu1 = {
+    RA: "0031432412012",
+    Nome: "Ian"
+  };
+  
+var alu2 = new Object();
+  alu2.RA = "0031125131513";
+  alu2.Nome = "Bruno";
+
+var alu3 = {};
+  alu3.RA = "0078456712910";
+  alu3.Nome = "Margarete";
+
+ 
